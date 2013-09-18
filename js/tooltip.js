23eb6c6a00bc2a93f856f00e1e3890c4c359684e
Henrique Bissoli <emp_shad@hotmail.com>
2013-09-18 14:47:25 -0300

    Update tooltip.js

--- js/tooltip.js
+++ js/tooltip.js
@@ -1,23 +1,27 @@
 window.jQuery || document.write(unescape('%3Cscript src="http://jquery.com/jquery-wp-content/themes/jquery/js/jquery-1.9.1.min.js"%3E%3C/script%3E'))
-$.fn.tooltipFollow = function(tooltip, classe)
+$.fn.tooltipFollow = function(texto_tooltip)
 {
+	
+	
 	var div = $(this);
-		$(div).hover(function(e){
-		 $("body").append("<div class='"+classe+"'>"+tooltip+"</div>");
-		 $("."+classe).css({
-			 top : e.pageY - 25,
-			 left : e.pageX + 30
+		 $(div).hover(function(event){
+		 var tooltip_id = "tooltip_"+Date().getTime();
+		 $("body").append("<div id='"tooltip_id"'>"+texto_tooltip+"</div>");
+		 $("#"+tooltip_id).css({
+			 top:		event.pageY - 25,
+			 left:		event.pageX + 30,
+			 position:	"absolute",
+			 display:	"block"
 		 }).fadeIn(250);
- 
 	}, function(){
-		$("."+classe).remove();
-	}).mousemove(function(e){
-	$("."+classe).css({
-			 top : e.pageY - 25,
-			 left : e.pageX + 30
-		 })
+		$("#"+tooltip_id).remove();
+	}).mousemove(function(event){
+	$("#"+tooltip_id).css({
+		top			event.pageY - 25,
+		left			event.pageX + 30
+	});
    })
 }
 
 /* Modo de usar */
-$("a").tooltipFollow("Texto do tooltip", "tooltipClass");
+$("a").tooltipFollow("Texto do tooltip");
