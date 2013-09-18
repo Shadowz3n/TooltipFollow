c2370b6f1c182ff1778af62a1bfc86c2d6fdf866
Henrique Bissoli <emp_shad@hotmail.com>
2013-09-18 15:10:03 -0300

    Update tooltip.js

--- js/tooltip.js
+++ js/tooltip.js
@@ -11,6 +11,10 @@
 		top:  		-25,
 		left: 		+30
 	});
+	
+	$("a").tooltipFollow({
+		css: "margin:10px;border-radius:5px;z-index:999"
+	});
 */
 
 window.jQuery || document.write(unescape('%3Cscript src="http://jquery.com/jquery-wp-content/themes/jquery/js/jquery-1.9.1.min.js"%3E%3C/script%3E'))
@@ -20,14 +24,15 @@ $.fn.tooltipFollow = function(options){
 		background: 	"gray",
 		color:		"white",
 		top:  		-25,
-		left: 		+30
+		left: 		+30,
+		css:		""
 	}, options);
 	
 	var div = $(this);
 	if($(div).length > 0){
 		$(div).hover(function(event){
 			 var tooltip_id = "tooltip_"+Date().getTime();
-			 $("body").append("<div id='"tooltip_id"' style='background:"+opt.background+";color:"+opt.color+";padding:10px;'>"+opt.texto+"</div>");
+			 $("body").append("<div id='"tooltip_id"' style='background:"+opt.background+";color:"+opt.color+";padding:10px;"+opt.css+"'>"+opt.texto+"</div>");
 			 $("#"+tooltip_id).css({
 				 top:		event.pageY - opt.top,
 				 left:		event.pageX + opt.left,
