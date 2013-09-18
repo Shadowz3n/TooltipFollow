02c6169c0e3823da20ed57a2ae69a876badfcea2
Henrique Bissoli <emp_shad@hotmail.com>
2013-09-18 14:58:51 -0300

    Update tooltip.js

--- js/tooltip.js
+++ js/tooltip.js
@@ -1,7 +1,7 @@
 window.jQuery || document.write(unescape('%3Cscript src="http://jquery.com/jquery-wp-content/themes/jquery/js/jquery-1.9.1.min.js"%3E%3C/script%3E'))
 $.fn.tooltipFollow = function(options)
 {
-	$.extend({
+	var opt = $.extend({
 		texto: "Texto padrão";
 		tema: "background:gray;border:1px solid #f4f4f4;";
 	}, options);
@@ -9,7 +9,7 @@ $.fn.tooltipFollow = function(options)
 	var div = $(this);
 		 $(div).hover(function(event){
 		 var tooltip_id = "tooltip_"+Date().getTime();
-		 $("body").append("<div id='"tooltip_id"' style="+theme+">"+texto_tooltip+"</div>");
+		 $("body").append("<div id='"tooltip_id"' style="+opt.tema+">"+opt.texto+"</div>");
 		 $("#"+tooltip_id).css({
 			 top:		event.pageY - 25,
 			 left:		event.pageX + 30,
